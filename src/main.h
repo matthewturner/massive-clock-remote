78456df8d43a71104a6443ec760f8cc06d4c05d2
Matt Turner <matt@fiverings.uk>
2022-04-30 16:51:47 +0100

    Convert to state machine (#1)

--- src/main.h
+++ src/main.h
@@ -13,23 +13,14 @@ const byte RECEIVE_PIN = 6;
 const byte SHOW_PIN = 2;
 
 const byte IDLE = 0;
-const byte PENDING = 1;
-const byte IN_PROGRESS = 2;
-
-volatile byte state = IDLE;
-volatile byte *pState = &state;
+const byte SENDING = 1;
 
 EvtManager mgr;
+StateMachineListener stateMachine;
 SoftwareSerial bluetoothSerial(RECEIVE_PIN, TRANSMIT_PIN);
 
-EvtByteListener *showTemporarilyListener;
-EvtTimeListener *returnToNormalListener;
-EvtByteListener *sleepListener;
-
-void setState(byte state);
-void wakeup();
-bool sleep();
-bool showTemporarily();
-bool returnToNormal();
+void onInterrupt();
+bool idle();
+bool sending();
 
 #endif
