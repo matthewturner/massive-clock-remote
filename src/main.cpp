5801aee8d04bd65732d9dd7c8afedd9b451f39bf
Matt Turner <matt@fiverings.uk>
2022-04-30 12:41:13 +0100

    Convert to state machine

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,36 @@ void setup()
   while (!Serial)
     ;
 
-  sleepListener = new EvtByteListener(pState, IDLE, (EvtAction)sleep);
-  mgr.addListener(sleepListener);
+  stateMachine.transition(IDLE);
 
-  showTemporarilyListener = new EvtByteListener(pState, PENDING, (EvtAction)showTemporarily);
-  mgr.addListener(showTemporarilyListener);
+  stateMachine.when(IDLE, (EvtAction)idle);
+  stateMachine.when(SENDING, (EvtAction)sending, IDLE);
 
-  returnToNormalListener = new EvtTimeListener(SHOW_TEMPORARILY_DURATION, true, (EvtAction)returnToNormal);
-  returnToNormalListener->disable();
-  mgr.addListener(returnToNormalListener);
+  stateMachine.whenInterrupted(IDLE, SENDING);
 
-  attachInterrupt(digitalPinToInterrupt(SHOW_PIN), wakeup, LOW);
+  mgr.addListener(&stateMachine);
+
+  attachInterrupt(digitalPinToInterrupt(SHOW_PIN), onInterrupt, FALLING);
 
   Serial.println(F("Setup complete. Continuing..."));
+}
+
+bool idle()
+{
+  Serial.println(F("Sleeping..."));
+  Serial.flush();
+  LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                SPI_OFF, USART0_OFF, TWI_OFF);
+  return true;
+}
+
+bool sending()
+{
+  Serial.println(F("Sending command..."));
+
+  bluetoothSerial.println(F(">show!"));
+
+  return true;
 }
 
 void loop()
@@ -33,59 +50,7 @@ void loop()
   mgr.loopIteration();
 }
 
-void wakeup()
+void onInterrupt()
 {
-  if (state == IDLE)
-  {
-    setState(PENDING);
-  }
-}
-
-bool sleep()
-{
-  Serial.println(F("Sleeping..."));
-  Serial.flush();
-  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
-                SPI_OFF, USART0_OFF, TWI_OFF);
-  return true;
-}
-
-void setState(byte newState)
-{
-  Serial.print(F("Setting state: "));
-  switch (newState)
-  {
-  case PENDING:
-    Serial.println(F("PENDING"));
-    break;
-  case IN_PROGRESS:
-    Serial.println(F("IN_PROGRESS"));
-    break;
-  case IDLE:
-    Serial.println(F("IDLE"));
-    break;
-  }
-  state = newState;
-}
-
-bool showTemporarily()
-{
-  Serial.println(F("Showing temporarily..."));
-  setState(IN_PROGRESS);
-
-  bluetoothSerial.println(F(">show!"));
-
-  returnToNormalListener->enable();
-
-  return true;
-}
-
-bool returnToNormal()
-{
-  Serial.println(F("Returning to normal..."));
-  returnToNormalListener->disable();
-
-  setState(IDLE);
-
-  return true;
+  stateMachine.onInterrupt();
 }
